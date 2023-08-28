3edaedaad86b84c9ad0d738b58084c529b796db6
zoomin3022 <zoomin3022@gmail.com>
2023-08-28 19:27:55 +0900

    conf: CapstoneDesignApplication에 timeZone 세팅과 @ConfigurationPropertiesScan 추가

--- capstone-design/src/main/java/com/inha/capstonedesign/CapstoneDesignApplication.java
+++ capstone-design/src/main/java/com/inha/capstonedesign/CapstoneDesignApplication.java
@@ -2,9 +2,19 @@ package com.inha.capstonedesign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CapstoneDesignApplication {
+
+	@PostConstruct
+	public void timeZoneSetting() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneDesignApplication.class, args);
