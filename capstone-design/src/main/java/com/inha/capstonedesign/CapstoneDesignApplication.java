77dbb61a879c90fe19db4a87e4af2b313290fc37
zoomin3022 <zoomin3022@gmail.com>
2023-09-03 20:53:14 +0900

    feat: 생성일을 기록하는 BaseEntity 기능 구현

--- capstone-design/src/main/java/com/inha/capstonedesign/CapstoneDesignApplication.java
+++ capstone-design/src/main/java/com/inha/capstonedesign/CapstoneDesignApplication.java
@@ -3,12 +3,14 @@ package com.inha.capstonedesign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class CapstoneDesignApplication {
 
 	@PostConstruct
