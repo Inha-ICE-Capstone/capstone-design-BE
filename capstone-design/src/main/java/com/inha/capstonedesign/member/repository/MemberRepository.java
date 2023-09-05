13a07cc8c4f5498e35106096fa5f8b9f17a8a526
zoomin3022 <zoomin3022@gmail.com>
2023-09-05 21:38:22 +0900

    feat: GET /members 내정보보기 기능 추가

--- capstone-design/src/main/java/com/inha/capstonedesign/member/repository/MemberRepository.java
+++ capstone-design/src/main/java/com/inha/capstonedesign/member/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.inha.capstonedesign.member.repository;
 import com.inha.capstonedesign.member.entity.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,4 +13,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @EntityGraph(attributePaths = {"roles"})
     Optional<Member> findByMemberEmail(String memberEmail);
+
+    @Query("SELECT m From Member m join fetch m.memberImage where m.memberEmail = :memberEmail")
+    Optional<Member> findByMemberWithImage(String memberEmail);
 }
--- capstone-design/src/main/java/com/inha/capstonedesign/member/service/MemberService.java
+++ capstone-design/src/main/java/com/inha/capstonedesign/member/service/MemberService.java
@@ -1,5 +1,10 @@
 package com.inha.capstonedesign.member.service;
 
+import com.inha.capstonedesign.auth.exception.AuthException;
+import com.inha.capstonedesign.auth.exception.AuthExceptionType;
+import com.inha.capstonedesign.member.dto.response.MemberResponseDto;
+import com.inha.capstonedesign.member.entity.Member;
+import com.inha.capstonedesign.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,5 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class MemberService {
 
+    private final MemberRepository memberRepository;
 
+    public MemberResponseDto getMyInfo(String email) {
+        final Member member = memberRepository.findByMemberWithImage(email)
+                .orElseThrow(() -> new AuthException(AuthExceptionType.ACCOUNT_NOT_EXISTS));
+
+        return MemberResponseDto.of(member);
+
+    }
 }
