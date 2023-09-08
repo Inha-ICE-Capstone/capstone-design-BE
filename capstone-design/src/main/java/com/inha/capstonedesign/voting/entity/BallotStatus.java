62e93cd3b1342798c5ef56dceac17a06613f212d
zoomin3022 <zoomin3022@gmail.com>
2023-09-08 22:36:09 +0900

    refactor: BallotStatus 좀 더 간단하게 korean 축약

--- capstone-design/src/main/java/com/inha/capstonedesign/voting/entity/BallotStatus.java
+++ capstone-design/src/main/java/com/inha/capstonedesign/voting/entity/BallotStatus.java
@@ -2,9 +2,9 @@ package com.inha.capstonedesign.voting.entity;
 
 public enum BallotStatus {
 
-    NOT_STARTED("아직 시작 안한 투표"),
-    IN_PROGRESS("진행중인 투표"),
-    CLOSED("마감된 투표");
+    NOT_STARTED("시작전"),
+    IN_PROGRESS("진행중"),
+    CLOSED("마감");
 
     private String korean;
 
