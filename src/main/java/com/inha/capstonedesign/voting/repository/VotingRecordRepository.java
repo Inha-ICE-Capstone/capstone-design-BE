ab438addcba187b99aa040db285e3ace05549d7d
zoomin3022 <zoomin3022@gmail.com>
2023-10-16 16:48:49 +0900

    refactor: findByVoter 에서 ballot, ballot.ballotImage EntityGraph 로 한번에 불러오게 수정

--- src/main/java/com/inha/capstonedesign/voting/repository/VotingRecordRepository.java
+++ src/main/java/com/inha/capstonedesign/voting/repository/VotingRecordRepository.java
@@ -5,6 +5,7 @@ import com.inha.capstonedesign.voting.entity.Ballot;
 import com.inha.capstonedesign.voting.entity.VotingRecord;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -12,5 +13,7 @@ import java.util.Optional;
 public interface VotingRecordRepository extends JpaRepository<VotingRecord, Long> {
 
     Optional<VotingRecord> findByVoterAndBallot(Member member, Ballot ballot);
+
+    @EntityGraph(attributePaths = {"ballot", "ballot.ballotImage"})
     Page<VotingRecord> findByVoter(Member member, Pageable pageable);
 }
