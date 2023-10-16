2d7da205db0cf19c427144c67fb4d82cc68efb35
zoomin3022 <zoomin3022@gmail.com>
2023-10-16 16:37:03 +0900

    feat: VotingRecordRepository 에 Page로 return하는 findByVoter 추가

--- src/main/java/com/inha/capstonedesign/voting/repository/VotingRecordRepository.java
+++ src/main/java/com/inha/capstonedesign/voting/repository/VotingRecordRepository.java
@@ -3,6 +3,8 @@ package com.inha.capstonedesign.voting.repository;
 import com.inha.capstonedesign.member.entity.Member;
 import com.inha.capstonedesign.voting.entity.Ballot;
 import com.inha.capstonedesign.voting.entity.VotingRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +12,5 @@ import java.util.Optional;
 public interface VotingRecordRepository extends JpaRepository<VotingRecord, Long> {
 
     Optional<VotingRecord> findByVoterAndBallot(Member member, Ballot ballot);
+    Page<VotingRecord> findByVoter(Member member, Pageable pageable);
 }
