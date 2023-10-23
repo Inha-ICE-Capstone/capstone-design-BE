3c25a89b99aa1c21f625e9907ecf5f8194caa604
zoomin3022 <zoomin3022@gmail.com>
2023-10-23 20:55:05 +0900

    refactor: 투표 목록 Page Size 3->6

--- src/main/java/com/inha/capstonedesign/global/config/PageSizeConfig.java
+++ src/main/java/com/inha/capstonedesign/global/config/PageSizeConfig.java
@@ -1,6 +1,6 @@
 package com.inha.capstonedesign.global.config;
 
 public class PageSizeConfig {
-    public static final int BALLOT_PAGE_SIZE = 3;
+    public static final int BALLOT_PAGE_SIZE = 6;
     public static final int BALLOT_RECORD_PAGE_SIZE=5;
 }
