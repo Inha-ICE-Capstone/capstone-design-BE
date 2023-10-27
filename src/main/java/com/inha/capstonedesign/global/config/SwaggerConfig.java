f294e0cf65e9cebb2ea8e1e7b4780814675c8026
zoomin3022 <zoomin3022@gmail.com>
2023-10-27 23:46:13 +0900

    feat: SwaggerConfig 추가

---
+++ src/main/java/com/inha/capstonedesign/global/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.inha.capstonedesign.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String BEARER = "bearer";
+    private static final String JWT = "JWT";
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .title("INHA 졸업 프로젝트 API")
+                .version("1.0.0")
+                .description("블록체인과 강화학습을 활용한 여론조사 플랫폼")
+                .contact(new Contact() // 연락처
+                        .name("zoomin3022")
+                        .email("jjwm0128@naver.com")
+                        .url("https://github.com/zoomin3022"));
+
+        // Security 스키마 설정
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(BEARER)
+                .bearerFormat(JWT)
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        // Security 요청 설정
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList(JWT);
+
+        return new OpenAPI()
+                // Security 인증 컴포넌트 설정
+                .components(new Components().addSecuritySchemes(JWT, bearerAuth))
+                // API 마다 Security 인증 컴포넌트 설정
+                .addSecurityItem(addSecurityItem)
+                .info(info);
+    }
+}
