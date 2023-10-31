4e9408574ac747fb0cf98ba99c34adca686d1a61
zoomin3022 <zoomin3022@gmail.com>
2023-10-31 16:17:59 +0900

    feat: global ErrorResponse, PageResponseDto 에 Swagger 적용

--- src/main/java/com/inha/capstonedesign/global/response/ErrorResponse.java
+++ src/main/java/com/inha/capstonedesign/global/response/ErrorResponse.java
@@ -1,11 +1,15 @@
 package com.inha.capstonedesign.global.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+@Schema(description = "오류시 반환 DTO")
 @Getter
 public class ErrorResponse {
 
+    @Schema(description = "오류 코드 (오류 코드표 참조)", example = "M001")
     private String code;
+    @Schema(description = "오류 메시지", example = "이미 가입된 이메일 입니다.")
     private String message;
 
     private ErrorResponse(String code, String message) {
--- src/main/java/com/inha/capstonedesign/global/response/PageResponseDto.java
+++ src/main/java/com/inha/capstonedesign/global/response/PageResponseDto.java
@@ -1,5 +1,6 @@
 package com.inha.capstonedesign.global.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,14 +8,20 @@ import org.springframework.data.domain.Page;
 
 import java.util.List;
 
+@Schema(description = "페이징 처리하고 반환하는 DTO")
 @Getter
 @Setter
 @NoArgsConstructor
 public class PageResponseDto<T> {
+    @Schema(description = "페이지 안에 들어갈 내용 list")
     private List<T> content;
+    @Schema(description = "총 페이지 수", example = "10")
     private int totalPages;
+    @Schema(description = "총 데이터 수", example = "27")
     private long totalElements;
+    @Schema(description = "현재 페이지 번호", example = "1")
     private int pageNumber;
+    @Schema(description = "한 페이지에 들어가는 데이터 수", example = "5")
     private int size;
 
     public PageResponseDto(Page<T> page) {
