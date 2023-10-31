04e78eb850305a5dc192a10f2c2d978e3c1f7ae1
zoomin3022 <zoomin3022@gmail.com>
2023-10-31 17:17:19 +0900

    feat: AdminMemberController 의 모든 api에 Swagger 적용

--- src/main/java/com/inha/capstonedesign/admin/controller/AdminMemberController.java
+++ src/main/java/com/inha/capstonedesign/admin/controller/AdminMemberController.java
@@ -1,6 +1,16 @@
 package com.inha.capstonedesign.admin.controller;
 
 import com.inha.capstonedesign.auth.service.AuthService;
+import com.inha.capstonedesign.global.response.ErrorResponse;
+import com.inha.capstonedesign.member.dto.response.MemberResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "admins", description = "관리자의 회원 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admins/members")
@@ -15,6 +26,13 @@ public class AdminMemberController {
 
     private final AuthService authService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content),
+            @ApiResponse(responseCode = "404", description = "실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
+    @Operation(summary = "관리자 권한 부여(임시 api)", description = "PathVariable로 받은 회원에게 관리자 권한 부여 \n지금은 임시 api로 이메일을 받지만 나중에 프론트 다 구현시 id로 받을 예정")
+    @Parameters({
+            @Parameter(name = "memberEmail", example = "test@gmail.com")
+    })
     @PostMapping("/authority/{memberEmail}")
     public ResponseEntity<String> upgradeAuthority(@PathVariable String memberEmail) {
         authService.upgradeAuthority(memberEmail);
