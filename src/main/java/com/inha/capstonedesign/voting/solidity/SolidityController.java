8aa8334fe9398c456d4e0d6daef70b1e23b3701b
zoomin3022 <zoomin3022@gmail.com>
2023-11-04 16:10:10 +0900

    feat: SolidityController 의 경우엔 Swagger 에 나오지 않게 Hidden처리

--- src/main/java/com/inha/capstonedesign/voting/solidity/SolidityController.java
+++ src/main/java/com/inha/capstonedesign/voting/solidity/SolidityController.java
@@ -1,5 +1,6 @@
 package com.inha.capstonedesign.voting.solidity;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/voting/solidity")
+@Hidden
 public class SolidityController {
 
     private final SolidityService solidityService;
