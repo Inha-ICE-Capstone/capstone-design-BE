336078b7e4b2cc57558a103ee89119d00dd83e24
zoomin3022 <zoomin3022@gmail.com>
2023-11-05 21:18:56 +0900

    fix: 다른 투표의 후보자도 투표가 가능했던 버그 해결

--- src/main/java/com/inha/capstonedesign/voting/repository/CandidateRepository.java
+++ src/main/java/com/inha/capstonedesign/voting/repository/CandidateRepository.java
@@ -1,5 +1,6 @@
 package com.inha.capstonedesign.voting.repository;
 
+import com.inha.capstonedesign.voting.entity.Ballot;
 import com.inha.capstonedesign.voting.entity.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
 
     Optional<Candidate> findByCandidateId(Long candidateId);
+
+    Optional<Candidate> findByCandidateIdAndBallot(Long candidateId, Ballot ballot);
 }
