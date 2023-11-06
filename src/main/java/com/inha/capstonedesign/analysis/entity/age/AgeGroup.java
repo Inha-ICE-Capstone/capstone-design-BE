0bfffc79f01be66224163fda12b994a201b63e2d
zoomin3022 <zoomin3022@gmail.com>
2023-11-06 20:14:43 +0900

    refactor: AgeGroup 한국어 내 띄어쓰기 제거

--- src/main/java/com/inha/capstonedesign/analysis/entity/age/AgeGroup.java
+++ src/main/java/com/inha/capstonedesign/analysis/entity/age/AgeGroup.java
@@ -1,13 +1,13 @@
 package com.inha.capstonedesign.analysis.entity.age;
 
 public enum AgeGroup {
-    TEENS_OR_LESS("10대 이하"),
+    TEENS_OR_LESS("10대이하"),
     TWENTIES("20대"),
     THIRTIES("30대"),
     FORTIES("40대"),
     FIFTIES("50대"),
     SIXTIES("60대"),
-    SEVENTIES_OR_ABOVE("70대 이상");
+    SEVENTIES_OR_ABOVE("70대이상");
 
     private String korean;
 
